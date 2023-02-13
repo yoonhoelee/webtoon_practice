65be222866d2e6845f21264825533dfe8986ea15
Adam Lee <70354575+yoonhoelee@users.noreply.github.com>
2023-02-13 17:20:45 +0900

    finished project

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:webtoon_practice/models/webtoon_detail_model.dart';
+import 'package:webtoon_practice/models/webtoon_episode_model.dart';
+import 'package:webtoon_practice/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+import '../widget/episode_widget.dart';
+
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
   const DetailScreen({
     super.key,
@@ -11,6 +16,51 @@ class DetailScreen extends StatelessWidget {
   });
 
   @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+  late SharedPreferences prefs;
+  bool isLiked = false;
+
+  Future initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (likedToons.contains(widget.id) == true) {
+        isLiked == true;
+      }
+    } else {
+      await prefs.setStringList('likedToons', []);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodeById(widget.id);
+    initPrefs();
+  }
+
+  onHeartTap() async {
+    final likedToons = prefs.getStringList('likedToons');
+    if (likedToons != null) {
+      if (isLiked) {
+        likedToons.remove(widget.id);
+      } else {
+        likedToons.add(widget.id);
+      }
+      await prefs.setStringList('likedToons', likedToons);
+      setState(() {
+        isLiked = !isLiked;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
@@ -18,39 +68,106 @@ class DetailScreen extends StatelessWidget {
         elevation: 2,
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        actions: [
+          IconButton(
+            onPressed: onHeartTap,
+            icon: Icon(
+              isLiked ? Icons.favorite : Icons.favorite_border_outlined,
+            ),
+          )
+        ],
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(
             fontSize: 24,
           ),
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 50,
+            vertical: 50,
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.3),
-                    )
-                  ],
-                ),
-                child: Image.network(thumb),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            blurRadius: 15,
+                            offset: const Offset(10, 10),
+                            color: Colors.black.withOpacity(0.3),
+                          )
+                        ],
+                      ),
+                      child: Image.network(widget.thumb),
+                    ),
+                  ),
+                ],
               ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: webtoon,
+                builder: ((context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text(
+                          '${snapshot.data!.genre}/${snapshot.data!.age}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                    );
+                  }
+                  return const Text("...");
+                }),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              FutureBuilder(
+                future: episodes,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Column(
+                      children: [
+                        for (var episode in snapshot.data!)
+                          Episode(
+                            episode: episode,
+                            webtoonId: widget.id,
+                          )
+                      ],
+                    );
+                  }
+                  return Container();
+                },
+              )
             ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
+import 'package:webtoon_practice/models/webtoon_detail_model.dart';
+import 'package:webtoon_practice/models/webtoon_episode_model.dart';
 import 'package:webtoon_practice/models/webtoon_model.dart';
 
 class ApiService {
@@ -17,6 +19,28 @@ class ApiService {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
       return webtoonInstances;
+    }
+    throw Error();
+  }
+  static Future<WebtoonDetailModel> getToonById(String id) async{
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await get(url);
+    if(response.statusCode == 200){
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodeById(String id) async{
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    List<WebtoonEpisodeModel> episodesInstances = [];
+    final response = await get(url);
+    if(response.statusCode == 200){
+      final episodes = jsonDecode(response.body);
+      for(var episode in episodes){  
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
     }
     throw Error();
   }
